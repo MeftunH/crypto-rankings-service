75201df7b1e1903297c5d38de04c787ac9101c05
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-16 22:24:57 +0300

    Init project

---
+++ src/test/java/com/crs/cryptorankingsservice/CryptoRankingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crs.cryptorankingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRankingsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
